bedb0053e85c5daaf6c788245979b5f27b9b7bfd
dakshdedha0001 <daksh_dedha23@manavrachna.net>
2026-08-15 11:18:14 +0530

    Updated Meta Pixel ID to 1091659590089819 in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.className} h-full antialiased`}>
       <head>
-        {/* Meta Pixel Code (Pixel ID: 3226577287526954) */}
+        {/* Meta Pixel Code (Pixel ID: 1091659590089819) */}
         <Script
           id="fb-pixel"
           strategy="afterInteractive"
@@ -41,7 +41,7 @@ export default function RootLayout({
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '3226577287526954');
+              fbq('init', '1091659590089819');
               fbq('track', 'PageView');
             `,
           }}
@@ -51,7 +51,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=3226577287526954&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1091659590089819&ev=PageView&noscript=1"
             alt="facebook-pixel-noscript"
           />
         </noscript>
